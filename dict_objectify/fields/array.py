513c9d97b670a9be7c0acb499c6f60e697c5f5b8
Dušan Reljić <reljicd@gmail.com>
2022-12-20 20:48:56 +0100

    Added Array class

---
+++ dict_objectify/fields/array.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+from typing import Generic, Iterable, List, Type, TypeVar
+
+from str2bool import str2bool
+
+from dict_objectify.base import Base
+from dict_objectify.constants import NONE_EQUIVALENT_VALUES
+from dict_objectify.fields.field_base import FieldBase
+
+T = TypeVar('T', str, bool, int, float, datetime, FieldBase)
+
+
+class Array(FieldBase, Generic[T]):
+    def __init__(self,
+                 model: Type[T],
+                 tag: str = None,
+                 *,
+                 primary: bool = False,
+                 nullable: bool = True) -> None:
+        super().__init__(tag, primary=primary, nullable=nullable)
+        if issubclass(model, FieldBase) and not issubclass(model, Base):
+            raise ValueError(f'The Array tag may use either primitives like '
+                             f'str, int, and float, or Base objects. '
+                             f'It may not currently use field types.'
+                             f'The type that was specified is {model}.')
+        self.model = model
+
+    def __get__(self, instance, owner) -> List[T]:
+        """ When the array is empty, None, or not present, it should be
+            returned as an empty array. """
+        if instance is not None:
+            result = super().__get__(instance, owner)
+            return result if result not in NONE_EQUIVALENT_VALUES else []
+        else:
+            return self
+
+    def __set__(self, instance, value: List[T]) -> None:
+        """ When a new value is empty or None the tag in the dictionary should
+            not be present at all. """
+        if value not in NONE_EQUIVALENT_VALUES:
+            if not isinstance(value, Iterable) or isinstance(value,
+                                                             (dict, str)):
+                # Most iterables should work,
+                # but not a dictionary or string.
+                raise ValueError(
+                    f'Tried to set Array tag [Tag: {self.tag}] with '
+                    f'a value [Value: {value}] of unsupported type '
+                    f'[Type: {type(value)}].')
+
+            processed_value = [v for v in value
+                               if v not in NONE_EQUIVALENT_VALUES]
+
+            if hasattr(self.model, 'to_dict'):
+                processed_value = [element.to_dict()
+                                   for element in processed_value]
+        else:
+            processed_value = value
+
+        super().__set__(instance, processed_value)
+
+    def _map(self, element: List) -> List[T]:
+        if isinstance(element, (list, set)):
+            elements = [e for e in element if e not in NONE_EQUIVALENT_VALUES]
+        else:
+            elements = element
+
+        if not elements:
+            return []
+
+        if self.model is str:
+            if not isinstance(elements[0], (str, int, float)):
+                raise ValueError(f'Expected list of types convertible to '
+                                 f'string, received value [Value: {element}] '
+                                 f'of type [Type: {type(elements[0])}] '
+                                 f'instead.')
+            mapped_element = [str(obj) for obj in elements]
+        elif self.model is bool:
+            if isinstance(elements[0], bool):
+                return elements
+            mapped_element = [str2bool(obj) for obj in elements]
+        elif self.model is int:
+            mapped_element = [int(obj) for obj in elements]
+        elif self.model is float:
+            mapped_element = [float(obj) for obj in elements]
+        elif isinstance(elements, (dict, str)):
+            mapped_element = [self.model(data_dict=elements)]
+        else:
+            mapped_element = [self.model(data_dict=data_dict)
+                              for data_dict in elements
+                              if data_dict not in NONE_EQUIVALENT_VALUES]
+
+        return [e for e in mapped_element if e not in NONE_EQUIVALENT_VALUES]
