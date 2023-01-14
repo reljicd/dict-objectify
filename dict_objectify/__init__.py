e2660c49fb7a3b99908259a9cd9dbe2c357bb62f
Dušan Reljić <reljicd@gmail.com>
2023-01-14 20:33:26 +0100

    Added EnumField

--- dict_objectify/__init__.py
+++ dict_objectify/__init__.py
@@ -1,6 +1,7 @@
 from dict_objectify.base import (Base, FieldBase, MixinBase,
                                  NONE_EQUIVALENT_VALUES, dict_base_hash)
-from dict_objectify.fields import Array, Bool, Datetime, Float, Integer, Text
+from dict_objectify.fields import (Array, Bool, Datetime, EnumField, Float,
+                                   Integer, Text)
 
 __version__ = '0.0.1'
 __all__ = [
@@ -12,6 +13,7 @@ __all__ = [
     Array,
     Bool,
     Datetime,
+    EnumField,
     Float,
     Integer,
     Text
